e39fa9e234eb2f66dca992e2ddb968fb047506e2
SahasPunchihewa <sahasmcg2995@gmail.com>
2021-03-20 23:04:42 +0530

    added logo and name

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var header=document.getElementById("header");
 var title=document.getElementById("nav-title");
 var anchor=document.getElementById("nav-anc");
 var mainTitle=document.getElementById("main-title");
+var headicon=document.getElementById("h-icon");
 
 var sticky=header.offsetTop;
 
@@ -15,6 +16,7 @@ function myFunction()
         title.classList.add("st-title");
         anchor.classList.add("st-anc");
         mainTitle.classList.add("main-sticky");
+        headicon.src="img/logo2.png";
     }
     else
     {
@@ -22,5 +24,6 @@ function myFunction()
         title.classList.remove("st-title");
         anchor.classList.remove("st-anc");
         mainTitle.classList.remove("main-sticky");
+        headicon.src="img/logo.png";
     }
 }
