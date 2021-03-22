fed35c5a7b403f3134cbe8e98f6d47269cf978e7
vizchamz <visalchamuditha@gmail.com>
2021-03-23 02:18:23 +0530

    svg diagram finished

---
+++ js/svg_diagram.js
@@ -0,0 +1,18 @@
+document.getElementById("groupId1").addEventListener("click", function() {
+    document.location.href="presentation.html";
+});
+document.getElementById("groupId3").addEventListener("click", function() {
+    document.location.href="index.html";
+});
+document.getElementById("groupId9").addEventListener("click", function() {
+    document.location.href="product_page.html";
+});
+document.getElementById("groupId10").addEventListener("click", function() {
+    document.location.href="quiz.html";
+});
+document.getElementById("groupId11").addEventListener("click", function() {
+    document.location.href="thumbnail_images.html";
+});
+document.getElementById("groupId12").addEventListener("click", function() {
+    document.location.href="student_details.html";
+});
