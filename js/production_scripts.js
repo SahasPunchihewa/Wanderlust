30df24a434b439549948489d88d086cf099493d2
SahasPunchihewa <sahasmcg2995@gmail.com>
2021-03-22 15:50:43 +0530

    added js for header

--- js/production_scripts.js
+++ js/production_scripts.js
@@ -138,3 +138,34 @@ function addProduct8(){
 		priceArray.push(productPrice8);	
 	}	
 }
+
+
+window.onscroll=function(){myFunction()};
+
+var header=document.getElementById("header");
+var title=document.getElementById("nav-title");
+var anchor=document.getElementById("nav-anc");
+var mainTitle=document.getElementById("main-title");
+var headicon=document.getElementById("h-icon");
+
+var sticky=header.offsetTop;
+
+function myFunction()
+{
+    if(window.pageYOffset>sticky)
+    {
+        header.classList.add("sticky");
+        title.classList.add("st-title");
+        anchor.classList.add("st-anc");
+        mainTitle.classList.add("main-sticky");
+        headicon.src="img/logo2.png";
+    }
+    else
+    {
+        header.classList.remove("sticky");
+        title.classList.remove("st-title");
+        anchor.classList.remove("st-anc");
+        mainTitle.classList.remove("main-sticky");
+        headicon.src="img/logo.png";
+    }
+}
