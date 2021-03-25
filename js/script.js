156c2c018a38d8f31ab293e571c78880ca676eba
SahasPunchihewa <sahasmcg2995@gmail.com>
2021-03-25 10:19:53 +0530

    added comments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
+//this file is to animate stickey header for main page
+
+//caaling function when scrolling
 window.onscroll=function(){myFunction()};
 
+//getting html elements
 var header=document.getElementById("header");
 var title=document.getElementById("nav-title");
 var anchor=document.getElementById("nav-anc");
 var mainTitle=document.getElementById("main-title");
 var headicon=document.getElementById("h-icon");
 var sticky=header.offsetTop;
+
+//function to change css clases
 function myFunction()
 {
     if(window.pageYOffset>sticky)
     {
+        //change to stickey classes
         header.classList.add("sticky");
         title.classList.add("st-title");
         anchor.classList.add("st-anc");
@@ -18,6 +25,7 @@ function myFunction()
     }
     else
     {
+        //chnage header to normal classes
         header.classList.remove("sticky");
         title.classList.remove("st-title");
         anchor.classList.remove("st-anc");
@@ -25,3 +33,8 @@ function myFunction()
         headicon.src="img/logo.png";
     }
 }
+
+/*Reference
+https://www.w3schools.com/howto/howto_js_sticky_header.asp
+
+*/
